336372cb7349f47428344aa216dd60c20410f09b
Aidan <aidan@p0ntus.com>
2024-12-19 20:42:32 -0500

    add typing to header

---
+++ src/public/js/typed.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    new Typed('#typed', {
+        strings: ["Aiden Charles", "Storm Chaser", "Technologist", "Aiden Charles"],
+        typeSpeed: 50,
+        backSpeed: 25,
+        loop: true,
+        showCursor: false
+    });
+});
