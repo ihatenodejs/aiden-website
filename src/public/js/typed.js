d61624b0b66b28497e8109961e8a7b26024b75c6
Aidan <aidan@p0ntus.com>
2024-12-19 22:06:26 -0500

    improve styling, add "chat" to home, change alignment on sidebar

--- src/public/js/typed.js
+++ src/public/js/typed.js
@@ -1,8 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     new Typed('#typed', {
-        strings: ["Aiden Charles", "Storm Chaser", "Technologist", "Aiden Charles"],
+        strings: [
+            "Storm Chaser",
+            "Technologist"
+        ],
         typeSpeed: 50,
         backSpeed: 25,
+        loop: true,
+        showCursor: false
+    });
+    new Typed('#intro', {
+        strings: [
+            "Hi, my name is Aiden! I'm 17, and I'm in the 11th grade.",
+            "I'm into storm chasing (my dream) and technology.",
+            "I appreciate you visiting my website and I hope you enjoy.",
+            "I included a couple of links below to get you started."
+        ],
+        typeSpeed: 50,
+        backSpeed: 25,
+        startDelay: 2000,
         loop: true,
         showCursor: false
     });
